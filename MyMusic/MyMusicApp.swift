4fb8f60c8eeeded600a0578e1f3e78c926aedb8e
hayashiyoshino <twinklestaryoshino@gmail.com>
2023-02-05 22:39:41 +0900

    Initial Commit

---
+++ MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by Yoshino Hayashi on 2023/02/05.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

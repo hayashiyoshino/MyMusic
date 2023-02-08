4a6efc1e212d2e63530a2734241f4be74a7149c2
hayashiyoshino <twinklestaryoshino@gmail.com>
2023-02-08 21:59:34 +0900

    feature: シンバル音源データの定数とシンバル用プレイヤーの変数を追加

--- MyMusic/SoundPlayer.swift
+++ MyMusic/SoundPlayer.swift
@@ -9,5 +9,10 @@ import UIKit
 import AVFoundation
 
 class SoundPlayer: NSObject {
-
+    // シンバルの音源データを読み込み
+    let cymbalData = NSDataAsset(name: "cymbalSound")!.data
+    
+    // シンバル用プレイヤーの変数
+    var cymbalPlayer: AVAudioPlayer!
+    }
 }

8f531f4862bbbec12fc0b8d1e0c2cdf6aa07e0b1
hayashiyoshino <twinklestaryoshino@gmail.com>
2023-02-10 22:35:27 +0900

    feature: BackgroundView.swift ファイル追加

---
+++ MyMusic/BackgroundView.swift
@@ -0,0 +1,20 @@
+//
+//  BackgroundView.swift
+//  MyMusic
+//
+//  Created by Yoshino Hayashi on 2023/02/10.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BackgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundView()
+    }
+}
